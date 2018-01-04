f191ffaca79e5a68b9f482ac475ce01b3d82af96
Bruno Krebs <bruno.krebs@auth0.com>
2018-01-04 18:08:30 -0200

    adding properties to canvas; adding two circles to canvas;

--- src/Canvas/Canvas.jsx
+++ src/Canvas/Canvas.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Canvas.css';
 
-const Canvas = () => (
-  <svg className="bang-bang-canvas" />
-);
+const Canvas = (props) => {
+  const viewBox = '0, 0, 1600, 800';
+  return (
+    <svg className="bang-bang-canvas" viewBox={viewBox} preserveAspectRatio="xMaxYMax meet">
+      {props.children}
+    </svg>
+  );
+};
+
+Canvas.propTypes = {
+  children: PropTypes.node,
+};
+
+Canvas.defaultProps = {
+  children: [],
+};
 
 export default Canvas;
