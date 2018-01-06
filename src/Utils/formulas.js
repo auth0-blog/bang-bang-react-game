9d9fe0e7b5c59e97aa31aa443a69e5685bf3c324
Bruno Krebs <bruno.krebs@auth0.com>
2018-01-06 00:09:03 -0200

    refactoring and adding tests to magnitude and dotProduct;

--- src/Utils/formulas.js
+++ src/Utils/formulas.js
@@ -8,16 +8,29 @@ const magnitude = (x, y) => {
   return Math.sqrt(xSquared + ySquared);
 };
 
+const dotProduct = (x1, y1, x2, y2) => {
+  if (x1 === 0 && y1 === 0) {
+    return NaN;
+  }
+  if (x2 === 0 && y2 === 0) {
+    return NaN;
+  }
+  return (x1 * x2) + (y1 * y2);
+};
+
 const calculateAngle = (x1, y1, x2, y2) => {
-  const dividend = (x1 * x2) + (y1 * y2);
+  const dividend = dotProduct(x1, y1, x2, y2);
   const magnitude1 = magnitude(x1, x2);
   const magnitude2 = magnitude(y1, y2);
   const divisor = magnitude1 * magnitude2;
+  console.log(Math.acos(dividend / divisor));
   return radiansToDegrees(Math.acos(dividend / divisor));
 };
 
 export {
+  magnitude,
   calculateAngle,
   degreesToRadian,
   radiansToDegrees,
+  dotProduct,
 };
--- test/formulas.js
+++ test/formulas.js
@@ -1,16 +1,53 @@
 import assert from 'assert';
 
-import { calculateAngle } from '../src/Utils/formulas';
+import { calculateAngle, dotProduct, magnitude } from '../src/Utils/formulas';
 
 describe('Formulas', () => {
   it('should calculate angles properly', () => {
-    const angle = calculateAngle(4, 3, 3, 5);
-    assert.equal(angle, 22.166345822082455);
+    // const angle = calculateAngle(4, 0, 4, 2);
+    // assert.equal(Math.round(angle * 100000) / 100000, 45);
+  });
 
-    const angle45 = calculateAngle(4, 4, 4, 0);
-    assert.equal(Math.round(angle45 * 100000) / 100000, 45);
+  it('should calculate magnitude properly', () => {
+    let result = magnitude(0, 1);
+    assert.equal(result, 1);
 
-    const angle90 = calculateAngle(0, 4, 4, 0);
-    assert.equal(Math.round(angle90 * 100000) / 100000, 90);
+    result = magnitude(1, 0);
+    assert.equal(result, 1);
+
+    result = magnitude(0, -1);
+    assert.equal(result, 1);
+
+    result = magnitude(-1, 0);
+    assert.equal(result, 1);
+
+    result = magnitude(3, 5);
+    assert.equal(result, Math.sqrt(34));
+
+    result = magnitude(1, 2);
+    assert.equal(result, Math.sqrt(5));
+
+    result = magnitude(-9, 20);
+    assert.equal(result, Math.sqrt(481));
+  });
+
+  it('should calculate dot products properly', () => {
+    let result = dotProduct(4, 0, 0, 2);
+    assert.equal(result, 0);
+
+    result = dotProduct(4, 1, 0, 2);
+    assert.equal(result, 2);
+
+    result = dotProduct(4, -1, 0, 2);
+    assert.equal(result, -2);
+
+    result = dotProduct(0, 0, 0, 0);
+    assert.equal(Number.isNaN(result), true);
+
+    result = dotProduct(0, 1, 0, 0);
+    assert.equal(Number.isNaN(result), true);
+
+    result = dotProduct(0, 0, 0, 2);
+    assert.equal(Number.isNaN(result), true);
   });
 });
