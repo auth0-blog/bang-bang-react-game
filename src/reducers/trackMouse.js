f04879d5925145daf1e2833c0c532c1df3275099
Bruno Krebs <bruno.krebs@auth0.com>
2018-01-16 10:00:32 -0200

    fixing bug with mobile;

--- src/reducers/trackMouse.js
+++ src/reducers/trackMouse.js
@@ -1,6 +1,7 @@
 import { calculateAngle } from '../utils/formulas';
 
 function trackMouse(state, action) {
+  if (!action.mousePosition) return state;
   const { x, y } = action.mousePosition;
   const angle = calculateAngle(0, 0, x, y);
   return {
