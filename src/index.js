1926819d77c0bbb6a96b750140f11010b8655f42
Bruno Krebs <bruno.krebs@auth0.com>
2018-01-16 18:15:43 -0200

    enabling redux on Dev Tools;

--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@ import Game from './containers/Game';
 import reducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer);
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  reducer, /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+/* eslint-enable */
 
 ReactDOM.render(
   <Provider store={store}>
