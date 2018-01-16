8d49b8bcf362e952ec1451d462229fa7d0e4f930
Bruno Krebs <bruno.krebs@auth0.com>
2018-01-16 20:28:35 -0200

    refactoring start game;

--- src/components/StartGame/StartGame.jsx
+++ src/components/StartGame/StartGame.jsx
@@ -2,29 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const StartGame = (props) => {
-  const buttonStyle = {
-    fill: '#ccc',
-    cursor: 'pointer',
+  const button = {
+    x: -145, // half width
+    y: -250, // minus means up (above 0)
+    width: 290,
+    height: 70,
+    rx: 10, // border radius
+    ry: 10, // border radius
+    style: {
+      fill: '#ccc',
+      cursor: 'pointer',
+    },
+    onClick: props.onClick,
   };
-  const textStyle = {
-    fontFamily: 'Verdana',
-    fontSize: '35',
-    fill: '#555',
-    cursor: 'pointer',
+
+  const text = {
+    textAnchor: 'middle', // center
+    x: 0, // center relative to X axis
+    y: -200, // 200 up
+    style: {
+      fontFamily: 'Verdana',
+      fontSize: 35,
+      fill: '#555',
+      cursor: 'pointer',
+    },
+    onClick: props.onClick,
   };
   return (
     <g filter="url(#big-shadow)">
-      <rect
-        style={buttonStyle}
-        x={-145}
-        y={-245}
-        width={290}
-        height={70}
-        rx={10}
-        ry={10}
-        onClick={props.onClick}
-      />
-      <text textAnchor="middle" x="0" y="-198" style={textStyle} onClick={props.onClick}>
+      <rect {...button} />
+      <text {...text}>
         New Game
       </text>
     </g>
